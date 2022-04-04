714e7668d241c0a4460f302ea79cfa0fc1e9df8d
Felipe Cabral <38699509+fkbral@users.noreply.github.com>
2022-04-04 15:37:23 -0300

    feat: aula 07 - fazendo requisições http na submissão de formulários

--- 02_projeto-react-typescript/src/pages/AnimalFacts/index.tsx
+++ 02_projeto-react-typescript/src/pages/AnimalFacts/index.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react"
 
+type Fact = {
+  _id: string,
+  text: string,
+  updatedAt: Date,
+}
+
 export function AnimalFacts() {
-  const [defaultFacts, setDefaultFacts] = useState([])
+  // const [defaultFacts, setDefaultFacts] = useState([] as Fact[])
+  const [defaultFacts, setDefaultFacts] = useState<Fact[]>([])
+  const [facts, setFacts] = useState<Fact[]>([])
   // let defaultFacts = []
   const [searchedAnimalType, setSearchedAnimalType] = useState('') 
+  const [animalType, setAnimalType] = useState('') 
 
-  async function getRandomFact() {  
+  async function getDefaultRandomFacts() {  
     const response = await fetch(
       `https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=2`
     )
@@ -16,32 +25,69 @@ export function AnimalFacts() {
     // defaultFacts = retrievedFacts
   }
 
+  async function getRandomFacts(animal_type: string = 'dog') {  
+    const response = await fetch(
+      `https://cat-fact.herokuapp.com/facts/random?animal_type=${animal_type.toLowerCase()}&amount=5`
+    )
+  
+    const retrievedFacts = await response.json()
+
+    setAnimalType(searchedAnimalType)
+    setFacts(retrievedFacts)
+    // defaultFacts = retrievedFacts
+  }
+
+  function handleSubmitAnimalFactSearch(
+    event: React.FormEvent<HTMLFormElement>
+  ) {
+    event.preventDefault()
+    getRandomFacts(searchedAnimalType)
+  }
+
   useEffect(() => {
-    getRandomFact()
+    getDefaultRandomFacts()
   }, [])
 
   return (
     <>
       <h1>Animal</h1>
-      <h2>Exemplos de fatos</h2>
+      <h2>Example dog facts</h2>
       <ul>
         {defaultFacts.map(fact => {
           return (
-          <li>
+          <li key={fact._id}>
             <p>{fact.text}</p>
+            <strong>{fact.updatedAt}</strong>
           </li>
           )})}
       </ul>
 
-      <h2>Pesquise novas curiosidades!</h2>
+      <h2>Search for new facts!</h2>
 
-      <label htmlFor="animal-type">Tipo do animal</label>
-      <input 
-        type="text"
-        id="animal-type"
-        value={searchedAnimalType}
-        onChange={(event) => setSearchedAnimalType(event.target.value)}
-      />
+      <form onSubmit={handleSubmitAnimalFactSearch}>
+        <label htmlFor="animal-type">Animal Type</label>
+        <input 
+          type="text"
+          id="animal-type"
+          value={searchedAnimalType}
+          onChange={(event) => setSearchedAnimalType(event.target.value)}
+          />
+
+        <button>Search for facts</button>
+      </form>
+
+      <h2>{animalType} facts</h2>
+      <ul>
+        {facts.length > 0 
+          ? facts.map(fact => {
+            return (
+            <li key={fact._id}>
+              <p>{fact.text}</p>
+            </li>
+            )})
+          : <p>No facts found for <strong>{animalType}</strong></p>
+        }
+      </ul>
     </>
   )
 }
